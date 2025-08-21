b35cf5582ab64046ee3f6aef67d2f2a746fb06d8
EstevanMelo11 <estevan11melo@gmail.com>
2025-08-21 22:20:14 +0000

    Desafio Modo: Aventureiro

--- SuperTrunfo.c
+++ SuperTrunfo.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(){
 
@@ -14,13 +15,16 @@ int main(){
     //Área de pedido e entrada de dados :
         //Char : 
     printf("Digite o Nome do Estado : ");
-    scanf("%s", &estado_A);
+    fgets(estado_A, 50, stdin);
+    estado_A[strcspn(estado_A,"\n")] = 0;
 
     printf("Digite o Código da Carta : ");
-    scanf("%s", &codigo_A);
+    fgets(codigo_A, 50, stdin);
+    codigo_A[strcspn(codigo_A,"\n")] = 0;
 
     printf("Digite o Nome da Cidade : ");
-    scanf("%s", &cidade_A);
+    fgets(cidade_A, 50, stdin);
+    cidade_A[strcspn(cidade_A,"\n")] = 0;
     
         //Int :
     printf("Digite a População : ");
@@ -37,24 +41,30 @@ int main(){
     scanf("%f", &pib_A);
 
 
-
     //Carta B -------------------------------------------------------
     char estado_B [50], cidade_B [50], codigo_B [50];
     int pontos_turisticos_B, populacao_B;
     float area_B, pib_B;
 
-    printf("\n\n- _ - ' - _ - CARTA B - _ - ' - _ -\n\n");
+    printf("\n- _ - ' - _ - CARTA B - _ - ' - _ -\n\n");
 
     //Área de pedido e entrada de dados :
         //Char : 
+    printf("");
+    fgets(estado_B, 50, stdin);
+    estado_B[strcspn(estado_B,"\n")] = 0;
+
     printf("Digite o Nome do Estado : ");
-    scanf("%s", &estado_B);
+    fgets(estado_B, 50, stdin);
+    estado_B[strcspn(estado_B,"\n")] = 0;
 
     printf("Digite o Código da Carta : ");
-    scanf("%s", &codigo_B);
+    fgets(codigo_B, 50, stdin);
+    codigo_B[strcspn(codigo_B,"\n")] = 0;
 
     printf("Digite o Nome da Cidade : ");
-    scanf("%s", &cidade_B);
+    fgets(cidade_B, 50, stdin);
+    cidade_B[strcspn(cidade_B,"\n")] = 0;
     
         //Int :
     printf("Digite a População : ");
@@ -70,6 +80,19 @@ int main(){
     printf("Digite o PIB : ");
     scanf("%f", &pib_B);
     
+    //Calculos:
+        //Carta A :
+    float densidade_populacional_A, pib_per_capita_A;
+
+    densidade_populacional_A = populacao_A / area_A;
+    pib_per_capita_A = pib_A / populacao_A;
+
+        //Carta B :
+    float densidade_populacional_B, pib_per_capita_B;
+
+    densidade_populacional_B = populacao_B / area_B;
+    pib_per_capita_B = pib_B / populacao_B;
+
     //Área de apresentação de dados coletados :
 
         //Dados da Carta A :
@@ -81,7 +104,9 @@ int main(){
     printf("População : %d\n", populacao_A);
     printf("Quantidade de Pontos Turísticos : %d\n", pontos_turisticos_A);
     printf("Área : %.2f Km²\n", area_A);
-    printf("PIB : %.2f\n\n", pib_A);
+    printf("PIB : R$ %.2f\n", pib_A);
+    printf("Densidade Populacional : %.2f Hab/Km²\n", densidade_populacional_A);
+    printf("PIB per Capita : R$ %.2f\n\n", pib_per_capita_A);
 
         //Dados da Carta B :
     printf("\n\n- - - - - - - CARTA : %s - - - - - - -\n\n", estado_B);
@@ -92,7 +117,10 @@ int main(){
     printf("População : %d\n", populacao_B);
     printf("Quantidade de Pontos Turísticos : %d\n", pontos_turisticos_B);
     printf("Área : %.2f Km²\n", area_B);
-    printf("PIB : %.2f\n\n", pib_B);
+    printf("PIB : %.2f\n", pib_B);
+    printf("Densidade Populacional : %.2f Hab/Km²\n", densidade_populacional_B);
+    printf("PIB per Capita : R$ %.2f\n\n", pib_per_capita_B);
+    
 
     return 0;
 }
